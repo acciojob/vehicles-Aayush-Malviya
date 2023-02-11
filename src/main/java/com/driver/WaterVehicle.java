213db6d328c01188e85e2197f1b672821e5e515b
Aayush-Malviya <aayushmalviya91@gmail.com>
2023-02-11 14:34:03 +0530

    commit

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,23 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+
+class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+    Boat(){
+        this.name = "";
+        this.capacity = 0;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+
+}
